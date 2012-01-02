9d9830a75adda6d52032a66e28f33dfa31d73918
Marty Nelson <marty.nelson@yahoo.com>
2012-01-02 01:19:37 -0800

    more if tests

--- photocopy-tests/if.js
+++ photocopy-tests/if.js
@@ -1,11 +1,19 @@
 require('../callback')
 var t = require('./test-functions')
 
+console.log.if(true)('true') 
+console.log.if(false)('false') 
 
+t.fn.if(true)('input', t.done)
+t.fn.if(false)('input', t.done)
 
+t.err.if(true)('input', t.done)
+t.err.if(false)('input', t.done)
 
-console.log.if(true)('true')
-console.log.if(false)('false')
 
 t.fn.if(true).else(t.elseFn)('input', t.done)
 t.fn.if(false).else(t.elseFn)('input', t.done)
+
+
+t.err.if(true).else(t.elseErr)('input', t.done)
+t.err.if(false).else(t.elseErr)('input', t.done)
